38970edc201a346e18c21c9f3d6ada67ad5c233e
Carlos Daniel <dev.cdfaria@gmail.com>
2021-08-31 22:42:09 -0300

    Refactoring

--- main.js
+++ main.js
@@ -1,27 +1,34 @@
-const range = document.getElementById('range')
+const Slider = {
+  range: document.getElementById('range'),
 
-range.addEventListener('input', (e) => {
-  const value = +e.target.value
-  const label = e.target.nextElementSibling
+  scale(number, inMin, inMax, outMin, outMax) {
+    return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+  },
 
-  const range_width = getComputedStyle(e.target).getPropertyValue('width')
-  const label_width = getComputedStyle(label).getPropertyValue('width')
+  labelManipulator(event) {
+    const value = Number(event.target.value)
+    const label = event.target.nextElementSibling
 
-  const num_width = +range_width.substring(0, range_width.length - 2)
-  const num_label_width = +label_width.substring(0, label_width.length - 2)
+    const range_width = getComputedStyle(event.target).getPropertyValue('width')
+    const label_width = getComputedStyle(label).getPropertyValue('width')
 
-  const max = +e.target.max
-  const min = +e.target.min
+    const num_width = Number(range_width.substring(0, range_width.length - 2))
+    const num_label_width = Number(label_width.substring(0, label_width.length - 2))
 
-  const left = value * (num_width / max) - num_label_width / 2 + scale(value, min, max, 10, -10)
+    const max = Number(event.target.max)
+    const min = Number(event.target.min)
 
-  label.style.left = `${left}px`
-  
-  label.innerHTML = value
-})
+    const left = value * (num_width / max) - num_label_width / 2 + Slider.scale(value, min, max, 10, -10)
 
-function scale (number, inMin, inMax, outMin, outMax) {
-  return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+    label.style.left = `${left}px`
+    label.innerHTML = value
+  },
+
+  start() {
+    range.addEventListener('input', event => Slider.labelManipulator(event))
+  }
 }
+
+Slider.start()
 
 // https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
